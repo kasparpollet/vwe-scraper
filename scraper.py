1592f0471b94b21e465f7c66626bb0f0dc5b87f7
Kaspar <kasparpollet127@gmail.com>
2024-02-08 12:27:14 +0100

    some optimalisations

--- scraper.py
+++ scraper.py
@@ -1,6 +1,10 @@
+import sys
 import csv
 import requests
 from bs4 import BeautifulSoup
+
+save_to_file = False
+get_data = False
 
 
 class Scraper:
@@ -10,19 +14,21 @@ class Scraper:
     def __init__(
         self,
         url = 'https://www.unitedconsumers.com/brandstofprijzen',
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'} ,
-        filename = 'data.csv',
+        filename = 'data',
     ):
         self.url = url
-        self.headers = headers
         self.filename = filename
         self.fuel_data = []
         self.field_names = ['name', 'price', 'difference']
 
-    def run(self):
-        # TODO check if elements are renamed
-        soup = BeautifulSoup(self.__get_html(), 'html5lib')
+    def run(self) -> None:
+        with open(f'{self.filename}.html', 'r', newline='') as f:
+            soup = BeautifulSoup(f.read(), 'html5lib')
+
         table_rows = soup.find_all('div', attrs = {'class':'row table-row'})
+
+        if not table_rows:
+            raise SystemExit('HTML elements can not be find, check if the website is updated')
 
         for row in table_rows:
             fuel_type = self.__clean_text(row.find('a').text)
@@ -30,22 +36,32 @@ class Scraper:
             price = self.__clean_text(fuel_info[0].text)
             difference = self.__clean_text(fuel_info[1].text)
 
-            # TODO maybe make it a list, dubble info
             self.fuel_data.append({'name': fuel_type, 'price': price, 'difference': difference})
 
-    def __get_html(self):
-        # TODO check if url is still alive
-        r = requests.get(self.url)
-        return r.content
+    def get_html(self) -> bytes:
+        try:
+            r = requests.get(self.url)
+            r.raise_for_status()
 
-    def save_to_csv(self):
+            with open(f'{self.filename}.html', 'w', newline='') as f:
+                f.write(r.text)
+            return r.content
+
+        except requests.exceptions.HTTPError as e:
+            raise SystemExit(e) # Example bad request
+            
+        except requests.exceptions.ConnectionError as e:
+            raise SystemExit(e) # Example no internet
+
+    def save_to_csv(self) -> None:
         # TODO check if file is valid
-        with open(self.filename, 'w', newline='') as f: 
+        # TODO Add date to file name or row
+        with open(f'P{self.filename}.csv', 'w', newline='') as f: 
             writer = csv.DictWriter(f, fieldnames = self.field_names) 
             writer.writeheader() 
             writer.writerows(self.fuel_data) 
 
-    def __clean_text(self, text):
+    def __clean_text(self, text: str) -> str | float:
         cleaned_text = text.replace(' ', '')
         cleaned_text = cleaned_text.replace('\n', '')
         cleaned_text = cleaned_text.replace('€', '')
@@ -53,15 +69,44 @@ class Scraper:
 
         try:
             cleaned_text = float(cleaned_text)
-        except Exception as e:
-            # TODO make better
-            pass
+        except (TypeError, ValueError):
+            pass # The string should stay a string
 
         return cleaned_text
 
+def get_params() -> None:
+    # TODO add arg for file path
+    if len(sys.argv) > 1:
+        symbol = sys.argv[1]
+        if symbol == '-s':
+            save_to_file = True
+        if symbol == '-g':
+            get_data = True
+
+    elif len(sys.argv) > 2:
+        symbol = sys.argv[2]
+        if symbol == '-s':
+            save_to_file = True
+        if symbol == '-g':
+            get_data = True
+
+
+def scrape() -> dict:
+    scraper = Scraper()
+
+    if get_data:
+        scraper.get_html()
+
+    scraper.run()
+
+    if save_to_file:
+        scraper.save_to_csv()
+
+    return scraper.fuel_data
+
 
 if __name__ == '__main__':
-    scraper = Scraper()
-    scraper.run()
-    scraper.save_to_csv()
-    print(scraper.fuel_data)
+    get_params()
+
+    data = scrape()
+    print(data)
